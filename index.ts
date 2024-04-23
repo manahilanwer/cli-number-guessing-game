eee0de3448cc5a4344b7437263bb4b3408c77be8
manahilanwer <missrose1185@gmail.com>
2024-04-23 21:35:46 +0500

    cli guessing game

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\tWellcome to codewithmanahil - CLI Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Enter your guess(Number Limit from 1 to 5)",
+},
+]);
+
+if (answer.userGuessedNumber === randomNumber){
+    console.log("Congratulation ! you guess a correct number.")
+}
+else{
+    console.log("sorry, you guess a wrong number.");
+
+}
